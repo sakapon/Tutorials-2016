0f8322d7a2f753e56a95efc034595b2222cfc406
Keiho Sakapon <saka_pon@live.jp>
2016-04-18 22:33:50 +0900

    Add ViewModel class

---
+++ XAML/Bmi/BmiWpf2/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reactive.Linq;
+using Reactive.Bindings;
+
+namespace BmiWpf2
+{
+    public class MainViewModel
+    {
+        public Body Body { get; } = new Body();
+
+        public ReadOnlyReactiveProperty<double> BmiRectangleWidth { get; }
+        public ReadOnlyReactiveProperty<string> BmiRectangleFill { get; }
+
+        public MainViewModel()
+        {
+            BmiRectangleWidth = Body.Bmi
+                .Select(x => 5 * x)
+                .ToReadOnlyReactiveProperty();
+            BmiRectangleFill = Body.Bmi
+                .Select(x => x < 25 ? "#FF009900" : x < 40 ? "#FFDDDD00" : "#FFEE0000")
+                .ToReadOnlyReactiveProperty();
+        }
+    }
+}
