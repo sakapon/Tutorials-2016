c8cc94af316be0bddbe89361e8523f9764596e5d
Keiho Sakapon <saka_pon@live.jp>
2016-05-31 16:26:26 +0900

    Bind RenderTransformOrigin to Translation

--- Leap-v2/LeapTutorials/ImagePinchLeap/MainViewModel.cs
+++ Leap-v2/LeapTutorials/ImagePinchLeap/MainViewModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Windows;
-using System.Windows.Media.Media3D;
 using Reactive.Bindings;
 
 namespace ImagePinchLeap
@@ -14,6 +13,7 @@ namespace ImagePinchLeap
 
         public ReadOnlyReactiveProperty<Vector> Translation { get; }
         public ReadOnlyReactiveProperty<double> Scale { get; }
+        public ReadOnlyReactiveProperty<Point> TransformOrigin { get; }
 
         public MainViewModel()
         {
@@ -22,6 +22,9 @@ namespace ImagePinchLeap
                 .ToReadOnlyReactiveProperty();
             Scale = AppModel.DraggedDelta
                 .Select(v => Math.Pow(2, 0.02 * v.Z))
+                .ToReadOnlyReactiveProperty();
+            TransformOrigin = Translation
+                .Select(v => new Point((2880 - v.X) / 5760, (1800 - v.Y) / 3600))
                 .ToReadOnlyReactiveProperty();
         }
     }
