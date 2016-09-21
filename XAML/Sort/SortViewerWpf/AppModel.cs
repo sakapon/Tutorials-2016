d2bb74acbdddddd1f839a82189bb272c5f5c9b59
Keiho Sakapon <git@saka-pon.net>
2016-09-21 19:05:54 +0900

    Implement AppModel

--- XAML/Sort/SortViewerWpf/AppModel.cs
+++ XAML/Sort/SortViewerWpf/AppModel.cs
@@ -1,13 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SortViewerWpf
 {
     public class AppModel
     {
+        static readonly TimeSpan ComparisonsSpan = TimeSpan.FromSeconds(1 / 1000.0);
+
+        public int[] Numbers { get; private set; }
+        public int ComparisonsCount { get; private set; }
+
+        public void BubbleSort(int maxNumber)
+        {
+            Numbers = RandomHelper.ShuffleRange(1, maxNumber).ToArray();
+            ComparisonsCount = 0;
+
+            Task.Run(() =>
+            {
+                Thread.Sleep(500);
+                Numbers.QuickSort((x1, x2) =>
+                {
+                    Thread.Sleep(ComparisonsSpan);
+                    ComparisonsCount++;
+                    return x1.CompareTo(x2);
+                });
+            });
+        }
     }
 
     public static class RandomHelper
