7d3e5f23e42db868686dd31ec18032c6f9ff3819
Keiho Sakapon <git@saka-pon.net>
2016-09-21 18:12:02 +0900

    Add classes

---
+++ XAML/Sort/SortViewerWpf/AppModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortViewerWpf
+{
+    public class AppModel
+    {
+    }
+
+    public static class RandomHelper
+    {
+        static readonly Random Random = new Random();
+
+        public static IEnumerable<int> ShuffleRange(int start, int count)
+        {
+            var l = Enumerable.Range(start, count).ToList();
+
+            while (l.Count > 0)
+            {
+                var index = Random.Next(l.Count);
+                yield return l[index];
+                l.RemoveAt(index);
+            }
+        }
+    }
+}
