61c7d606289a467bd236dfc8f4e2d8f0c29288a9
Keiho Sakapon <git@saka-pon.net>
2018-06-25 15:44:37 +0900

    Add method

--- XAML/PostalCodes/PostalCodesWpf/HttpHelper.cs
+++ XAML/PostalCodes/PostalCodesWpf/HttpHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace PostalCodesWpf
 {
@@ -13,6 +14,17 @@ namespace PostalCodesWpf
                 var response = await http.GetAsync(uri);
                 response.EnsureSuccessStatusCode();
                 return await response.Content.ReadAsAsync<T>();
+            }
+        }
+
+        async public static Task<T> GetJsonAsync<T>(string uri)
+        {
+            using (var http = new HttpClient())
+            {
+                var response = await http.GetAsync(uri);
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<T>(content);
             }
         }
 
